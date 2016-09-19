bcfffc8d8e84359b9f23b1751198844b50b02783
[ßµ∟∟€♱› <danpoulin01@gmail.com>
2016-09-19 18:01:31 -0400

    Update out_of_seq_inode.sh

--- out_of_seq_inode.sh
+++ out_of_seq_inode.sh
@@ -16,26 +16,33 @@ usage () {
   echo "the inodes are out of sequence as will happen if system"
   echo "binaries are overwritten / time-stomped."
   exit 1
+}
+if [ $# -lt 1 ] ; then
+	usage
+fi
 
 read -p " Enter Directory containing Binaries suspected of 'Out-Of_Sequence Inodes/Time-Stomping' " PATH
 
+#output a listing sorted by inode number to a temp file:
 templs='/tmp/temp-ls.txt'
 ls -ali $PATH | sort -n > $templs
+
 # this is for discarding first couple lines in output
 foundfirstinode=false
 declare -i startinode
+
 while read line;
 do
-	if ["$foundfirstinode" = false] && ['\echo $line | wc -w\' -gt 6]; 
-	then
+#usually a couple lines of garbage at start of output; this supresses it
+	if ["$foundfirstinode" = false] && [ '\echo $line | wc -w\' -gt 6 ] ;	then
 		startinode=`expr $(echo $line | awk '{print $1}')`
 		echo "Start inode = $startinode"
 		foundfirstinode=true
 	fi
 	q=$(echo $line | awk '{print $1}')
-	if [[ $q =~ ^[0-9]+$ ]] && [ "$startinode" -lt $q ];
-	then
-		if [$((startinode + 2)) -lt $q ]; then
+	if [[ $q =~ ^[0-9]+$ ]] && [ "$startinode" -lt $q ] ;	then
+		
+		if [ $((startinode + 2)) -lt $q ]; then
 			echo -e "\e[31m****Out of Sequence inode detected**** expect $startinode got $q\e[0m"
 		else
 			echo -e "\e[33m****Out of Sequence inode detected**** expect $startinode got $q\e[0m"
@@ -46,7 +53,6 @@ do
 			startinode=$((startinode+1))
 done < $templs
 rm $templs
-}
 
 
 
