0d014b123a035bf46722e1461e759b211022487c
[ßµ∟∟€♱› <danpoulin01@gmail.com>
2016-09-19 23:25:40 -0400

    Update AUPNOTEs.sh

--- AUPNOTEs.sh
+++ AUPNOTEs.sh
@@ -39,18 +39,21 @@ A=$(cat /tmp/PS1_ROOT.txt)
 #B=user PS1
 B=$(cat /tmp/PS1_USER.txt)
 
-ROOT=$(sed -i "s/$Z/$A/" /root/.bashrc)
-USER=$(sed -i "s/$X/$B/" ~/.bashrc)
-
 if [ $id = 0 ] ; then
   Z=$(cat /root/.bashrc | grep -A1 '$color_prompt' | grep -v if)
+  ROOT=$(sed -i "s/$Z/$A/" /root/.bashrc)
   $ROOT
 elif [ $id = !0 ] ; then
   X=$(cat ~/.bashrc | grep -A1 '$color_prompt' | grep -v if)
+  USER=$(sed -i "s/$X/$B/" ~/.bashrc)
   $USER
 else
   exit
 fi
+
+#ROOT=$(sed -i "s/$Z/$A/" /root/.bashrc)
+#USER=$(sed -i "s/$X/$B/" ~/.bashrc)
+
 date=$(date +%d-%b-%Y\-%H\:%M\:%S)
 S=$(script -a /tmp/OPNOTES-${date}.txt)
 read -p "Would you like to commence OPNOTES now ?   y/n ? " T
