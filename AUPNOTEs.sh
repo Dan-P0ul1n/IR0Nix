81c64fac1265ebd2f21ba100c2edaef58ea32bc4
[ßµ∟∟€♱› <danpoulin01@gmail.com>
2016-09-19 20:01:14 -0400

    Update AUPNOTEs.sh

--- AUPNOTEs.sh
+++ AUPNOTEs.sh
@@ -2,9 +2,9 @@
 #IDs current logged on UID and assigns it to 'id'
 id=$(id | cut -d'=' -f2 | cut -d'(' -f1)
 #A=root PS1
-A=${PS1='${debian_chroot:+($debian_chroot)}\[\e[47m\]\[\033[01;31m\]$HANDLE\[\033[31m\] : \[\e[47m\]\[\033[01;31m\](\D{%d-%b-%Y - %H:%M:%S})\n\w\[\033[31m\]\ # '}
+A=${PS1='${debian_chroot:+($debian_chroot)}\[\e[47m\]\[\033[01;31m\]$HANDLE @ $BOX\[\033[31m\] : \[\e[47m\]\[\033[01;31m\](\D{%d-%b-%Y - %H:%M:%S})\n\w\[\033[31m\]\ # '}
 #B=user PS1
-B=${PS1='${debian_chroot:+($debian_chroot)}\[\033[0;36m\]$HANDLE\[\033[00m\] : \[\033[0;36m\](\D{%d-%b-%Y - %H:%M:%S})\n\w\[\033[00m\]\ $ '}
+B=${PS1='${debian_chroot:+($debian_chroot)}\[\033[0;36m\]$HANDLE @ $BOX\[\033[00m\] : \[\033[0;36m\](\D{%d-%b-%Y - %H:%M:%S})\n\w\[\033[00m\]\ $ '}
 C=$(read -p "What would you like your handle to be ?" HANDLE)
 D=$(read -p "What would you like to call this box ?" BOX)
 
